e88c238419ccebd7907afe55488d10dba1fffb59
Pablo Nevares <pablo@nevares.com>
2018-05-03 18:42:25 -0700

    Refactor fileserver, update package info

--- index.mjs
+++ index.mjs
@@ -1,21 +1,29 @@
-import http from 'http';
 import fs from 'fs';
+import http from 'http';
 import path from 'path';
+
+const root = 'lib';
 
 http.createServer((req, res) => {
   const { base, ext } = path.parse(req.url);
 
-  if (['index.html', 'script.js', 'hello.js'].includes(base)) {
-    fs.readFile(base, (error, data) => {
-      if (error) {
-        return res.end(JSON.stringify(error));
-      }
-
-      const mimetype = ext === '.js' ? 'text/javascript' : 'text/html';
-      res.setHeader('Content-Type', mimetype);
-      res.end(data);
-    });
-  } else {
-    res.end(`Invalid path: ${req.url}`);
+  let mimetype = 'text/plain';
+  switch(ext) {
+    case '.js':
+      mimetype = 'text/javascript';
+      break;
+    case '.html':
+      mimetype = 'text/html';
+      break;
   }
+
+  fs.readFile(path.join(root, base), (error, data) => {
+    if (error) {
+      res.statusCode = 404;
+      return res.end();
+    }
+
+    res.setHeader('Content-Type', mimetype);
+    res.end(data);
+  });
 }).listen(8080);
